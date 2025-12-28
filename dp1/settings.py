62158d4fcb1ebe398cafacabe4f195a600efec4e
vishal-gupta025 <vishaljee000@gmail.com>
2025-12-29 00:32:53 +0530

    Secure settings and cleanup gitignore artifacts

--- dp1/settings.py
+++ dp1/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-og1m_qk-pghy$1r8f_2%_gd@+u&qaka9l$w(^d16p@5vh$)n+1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
